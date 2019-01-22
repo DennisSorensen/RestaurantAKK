19f1e72299df24bd877b4d072cbeca9a4ed050df
Dennis Sørensen <dennissorensen24@hotmail.dk>
2019-01-22 12:02:53 +0100

    Initial Commit

---
+++ RestaurantAKK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantAKK
+//
+//  Created by Dennis Sørensen on 22/01/2019.
+//  Copyright © 2019 DennisSorensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
